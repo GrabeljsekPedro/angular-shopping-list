560bfe2128a3732321eb9e6f28df273d75a3bc5e
GrabeljsekPedro <grabeljsekpedro@gmail.com>
2023-03-31 00:47:10 +0200

    recipe model created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes = [];
 
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
